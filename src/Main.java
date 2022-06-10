c1848bfd0b1b8ed99d442abd7e14e56152920fa6
Scott Carpenter <carpenter.scott22@gmail.com>
2022-06-10 16:51:01 -0500

    answered twoSums problem in Java and Javascript

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+    run();
+
+
+    }
+
+
+    public static void runSolutions() {
+        int[] array = {1, 11, 6, 4, 9};
+        TwoSums(array, 10);
+    }
+
+}
