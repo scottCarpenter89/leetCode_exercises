d372cdb47b91f8560d649e9e025c8c5bb991c4ba
Scott Carpenter <carpenter.scott22@gmail.com>
2022-06-26 23:46:40 -0400

    completed binarySearch

--- js/binarySearch.js
+++ js/binarySearch.js
@@ -5,7 +5,10 @@
  */
 let search = function(nums, target) {
 // input: an array of numbers and a target number
-
-
-    // output:
+   return nums.indexOf(target);
+    // output: index of the found number or -1
 };
+
+
+console.log(search([0,1,5,8,16], 8));
+console.log(search([0,1,5,8,16], 2));
