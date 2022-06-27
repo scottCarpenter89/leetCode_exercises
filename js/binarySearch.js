50587783ac2d50d11fc3a2948b787b5df5b9c9e8
Scott Carpenter <carpenter.scott22@gmail.com>
2022-06-26 23:17:59 -0400

    began binarySearch algorithms

---
+++ js/binarySearch.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+let search = function(nums, target) {
+// input: an array of numbers and a target number
+
+
+    // output:
+};
