4c4e1e1fac59389f99e8b0f76b373dae58cadca3
Scott Carpenter <carpenter.scott22@gmail.com>
2022-09-15 21:40:49 -0500

    corrected syntax

--- js/running1dArraySum.js
+++ js/running1dArraySum.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var runningSum = function(nums) {
+let runningSum = function(nums) {
     let temp = 0;
     let sum;
     for (let i = 0; i < nums.length; i++) {
@@ -10,10 +10,10 @@ var runningSum = function(nums) {
         nums[i] = sum;
         temp = sum;
     }
-    console.log(nums)
+    console.log(nums);
     return nums;
 };
 
 runningSum([1,2,3,4]);
 runningSum([1,1,1,1]);
-runningSum([3,1,2,10,1])
+runningSum([3,1,2,10,1]);
